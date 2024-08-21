657b444a9415de1da6b2407dd4b01c076a1065bf
Zeeshan Ahmad <xeeshanahmad0201@gmail.com>
2024-08-21 11:56:48 +0500

    working on messaging module

--- backend/api/controllers/message_controller.go
+++ backend/api/controllers/message_controller.go
@@ -2,16 +2,18 @@ package controllers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/zeeshanahmad0201/chatify/backend/internal/message"
 	"github.com/zeeshanahmad0201/chatify/backend/models"
 )
 
 func StoreMessage(w http.ResponseWriter, r *http.Request) {
 	// extract payload
-	var message *models.Message
-	err := json.NewDecoder(r.Body).Decode(&message)
+	var messageObj *models.Message
+	err := json.NewDecoder(r.Body).Decode(&messageObj)
 	if err != nil {
 		http.Error(w, "invalid payload", http.StatusBadRequest)
 		return
@@ -19,10 +21,10 @@ func StoreMessage(w http.ResponseWriter, r *http.Request) {
 
 	// validate struct
 	validate := validator.New()
-	err = validate.Struct(&message)
+	err = validate.Struct(&messageObj)
 	if err != nil {
-		errs := err.(validator.ValidationErrors)
-		if len(errs) == 0 {
+		var errs validator.ValidationErrors
+		if !errors.As(err, &errs) {
 			http.Error(w, "invalid payload", http.StatusBadRequest)
 			return
 		}
@@ -38,5 +40,13 @@ func StoreMessage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// send/store message
-	err := 
+	err = message.StoreMessage(messageObj)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("message sent successfully!")
 }
